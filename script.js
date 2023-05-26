f163ea0f8a3bf84a2627684d393e32c6a2b8d64f
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-26 18:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your JS code here. If required.
 // prompt('DO you want to proceed?')
 if(prompt('Do you want to proceed?')==='confirm'){
-	alert("You clicked OK.Proceeding...");
+	alert("You clicked OK. Proceeding...");
+	
 }else{
 	alert("You clicked Cancel. Exiting...")
 }
+
+
 
 
